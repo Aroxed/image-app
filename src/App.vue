4a6e867a597299fb7ce13b8771212df409463e85
Aroxed <petrashenko@gmail.com>
2023-05-01 17:40:32 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="image-list">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="(image, index) in images" :key="index">
+        <img :src="image.url" alt="image.description">
+        <p>{{ image.description }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: 'My Image List',
+      images: [
+        {
+          url: 'https://via.placeholder.com/150/0',
+          description: 'This is the first image',
+        },
+        {
+          url: 'https://via.placeholder.com/150/2550255',
+          description: 'This is the second image',
+        },
+        {
+          url: 'https://via.placeholder.com/150/210012',
+          description: 'This is the third image',
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.image-list {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+img {
+  max-width: 100%;
+  margin-right: 1rem;
+}
+
+p {
+  margin: 0;
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,18 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe('App', () => {
+  test('renders the correct number of images', () => {
+    const wrapper = mount(App);
+    const images = wrapper.findAll('img');
+    expect(images).toHaveLength(3);
+  });
+
+  test('renders the correct image descriptions', () => {
+    const wrapper = mount(App);
+    const descriptions = wrapper.findAll('p');
+    expect(descriptions.at(0).text()).toBe('This is the first image');
+    expect(descriptions.at(1).text()).toBe('This is the second image');
+    expect(descriptions.at(2).text()).toBe('This is the third image');
+  });
+});
